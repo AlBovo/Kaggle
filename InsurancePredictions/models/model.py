acce9b4fa8462d9d9bea63997aa3f5dc314b600b
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-12-23 10:24:59 +0100

    finished first challenge and uploaded new one

--- InsurancePredictions/models/model.py
+++ InsurancePredictions/models/model.py
@@ -1,37 +1,27 @@
 import numpy as np
-import optuna
-from xgboost import XGBRegressor
-from sklearn.model_selection import cross_val_score
-from optuna.samplers import TPESampler
-from optuna.pruners import MedianPruner
+from sklearn.ensemble import RandomForestRegressor
 import pickle
 
 def train_model():
     features = np.load("features.npy")
     results = np.load("results.npy")
     
-    model = XGBRegressor(
-        n_estimators=189,
-        max_depth=32,
-        learning_rate=1.0,
-        subsample=0.8,
-        colsample_bytree=0.8,
-        num_parallel_tree=100,
-        booster="gbtree",
-        tree_method="hist",
-        device="gpu",
-        random_state=42
+    model = RandomForestRegressor(
+        n_estimators=386,
+        max_depth=7,
+        max_features="log2",
+        random_state=42,
     )
 
     model.fit(features, results)
 
-    with open("xgboost_model.pkl", "wb") as file:
+    with open("random_forest_model.pkl", "wb") as file:
         pickle.dump(model, file)
 
 
 def test_model():
     with open("random_forest_model.pkl", "rb") as file:
-        model: XGBRegressor = pickle.load(file)
+        model: RandomForestRegressor = pickle.load(file)
 
     test_f = np.load("test_f.npy")
     predictions = model.predict(test_f)
